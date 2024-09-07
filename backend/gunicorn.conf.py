675b5be30746ca4b399dd9864e46ff4c44626b03
Meet Patel <113581962+meetpateltech@users.noreply.github.com>
2024-09-07 10:22:05 +0530

    fix

--- backend/gunicorn.conf.py
+++
@@ -1,9 +0,0 @@
-import multiprocessing
-
-bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
-
-# Add these lines for logging
-errorlog = "/var/log/gunicorn.log"
-loglevel = "debug"
-capture_output = True

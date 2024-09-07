6b742f95e36c4f6e5d05e245b6d954ccd6d3d69f
Meet Patel <113581962+meetpateltech@users.noreply.github.com>
2024-09-07 09:57:03 +0530

    fix prod again

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -2,3 +2,8 @@ import multiprocessing
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
+
+# Add these lines for logging
+errorlog = "/var/log/gunicorn.log"
+loglevel = "debug"
+capture_output = True
